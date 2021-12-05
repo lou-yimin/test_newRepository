9a23c269b0a34bdff35ec498a4744a5684b24184
lou-yimin <1298632838@qq.com>
2021-12-05 12:14:47 +0800

    try

---
+++ Test/Untitled1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<list> 
+#include<cstring>//c++
+using namespace std;
+class Page {
+	public:
+	Page() {}
+	private:
+	char data_[3];
+	int page_id_ = 1;
+};
+int main()
+{
+	Page *page = nullptr;
+	cout<<page->page_id;
+	return 0;
+}
+
